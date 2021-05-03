243c33b0ea3b8fa9ca639335320956b4458a39f6
MAC <mac@MACui-MacBookPro.local>
2021-05-03 23:37:29 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -26,7 +26,18 @@ function login(){
         body: JSON.stringify(req),
     })
     .then((res)=>res.json())
-    .then((res)=>{});
+    .then((res)=>{
+        if(res.success){
+            location.href = "/";
+        }
+        else{
+            alert(res.msg);
+        }
+    })
+    .catch((err)=>{
+        //console.error(new Error('로그인 중 에러 발생'));
+        console.error(('로그인 중 에러 발생');
+    });
     
 }
 
