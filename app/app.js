845f08681fb68774082f9ebde2a4f4b087cc70c1
MAC <mac@MACui-MacBookPro.local>
2021-05-03 21:36:15 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const home = require('./src/routes/home');
+
+//앱 세팅
+app.set('views', './src/views');
+//엔진코드 해석 명시
+app.set('view engine','ejs');
+
+//미들웨어를 등록해주는 메서드
+app.use('/',home);
+
+module.exports = app;
+
+
+
+
+
+//http 형식으로 server webserver 실행
+/*
+const http = require('http');
+const app = http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'})
+
+
+    //console.log(req.url);
+    if(req.url === '/'){
+        res.end("여기는 루트입니다.");
+    } 
+    else if(req.url === '/login'){
+        res.end("여기는 로그인 화면입니다.");
+    }
+});
+
+app.listen(3001, ()=>{
+    console.log('http 로 가동된 서버 입니다.');
+});
+*/
