9b16c04ea93a782173264a05c87462a75b5097ac
MAC <mac@MACui-MacBookPro.local>
2021-05-03 21:45:25 +0900

    프론트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -8,6 +8,8 @@ const home = require('./src/routes/home');
 app.set('views', './src/views');
 //엔진코드 해석 명시
 app.set('view engine','ejs');
+//미들웨어 세팅
+app.use(express.static(`${__dirname}/src/public`));
 
 //미들웨어를 등록해주는 메서드
 app.use('/',home);
