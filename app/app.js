32e34978199a4f6fbb6f3cbfb1b5918285023717
MAC <mac@MACui-MacBookPro.local>
2021-05-03 23:16:08 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,18 +1,23 @@
 "use strict";
 
+//모듈
 const express = require('express');
+const bodyPaser = require('body-parser');
 const app = express();
+
+//라우팅
 const home = require('./src/routes/home');
 
 //앱 세팅
-app.set('views', './src/views');
-//엔진코드 해석 명시
-app.set('view engine','ejs');
-//미들웨어 세팅
+app.set('views', './src/views');//view 폴더위치
+app.set('view engine','ejs');//엔진코드 해석 명시
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyPaser.json());
+//URL을 통해서 전달되는 테이터에 한글,
+//공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제를 해결
+app.use(bodyPaser.urlencoded({ extended: true }));
 
-//미들웨어를 등록해주는 메서드
-app.use('/',home);
+app.use('/',home);//미들웨어를 등록해주는 메서드
 
 module.exports = app;
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,41 @@
 "use strict";
 
-const home = (req, res)=>{
-    res.render('home/index');
+const output = {
+    home : (req, res)=>{
+        res.render('home/index');
+    },    
+    login : (req,res)=>{
+        res.render('home/login');
+    },
 };
 
-const login = (req,res)=>{
-    res.render('home/login');
+const users = {
+    id: ["yoonit","나개발","윤팀장"],
+    psword: ["123","1234","12345"],
 };
 
+const process = {
+    login:(req,res) =>{
+        //console.log(req.body);
+        const id = req.body.id,
+         psword = req.body.psword;        
+        console.log(id,psword);
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx]===psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
+};
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
