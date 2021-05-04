1426c318cb7e7ff3a64f559b520c63b843837cfc
MAC <mac@MACui-MacBookPro.local>
2021-05-04 19:31:10 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home : (req, res)=>{
@@ -12,25 +12,11 @@ const output = {
 };
 
 const process = {
-    login:(req,res) =>{
-        //console.log(req.body);
-        const id = req.body.id,
-         psword = req.body.psword;       
-         
-        const users = UserStorage.getUser("id", "psword");
-
-        const response ={}; 
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx]===psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
-        return res.json(response);        
+    login:(req,res) =>{        
+        const user = new User(req.body);
+        const response = user.login();
+        //console.log(response);
+        return res.json(response);   
     },
 };
 
